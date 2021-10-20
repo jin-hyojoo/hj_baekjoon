2265677312b1c009dc6c4ca12e9ee85852b4fa77
jin-hyojoo <cuteseal97@gmail.com>
2021-10-21 04:42:11 +0900

    question title 'for'

---
+++ for/__init__.py
@@ -0,0 +1,56 @@
+'''
+# 2739번 구구단 출력
+num = int(input())
+for i in range(1,10):
+    print(f'{num} * {i} = {num*i}' )
+    
+# 10950번
+testcnt = int(input())
+for i in range(1,testcnt+1):
+    a,b = map(int, input().split())
+    print(a+b)
+
+# 8393번
+n, sum = int(input()), 0
+for i in range(1, n+1):
+    sum += i
+print(sum)
+
+# 15552번 빠른 A+B
+
+
+
+
+# 2741번
+num = int(input())
+for i in range(1,num+1): print(i)
+
+# 2741번 한 줄.ver
+for i in range(1, int(input())+1): print(i)
+
+# 2742번
+for i in range(int(input()), 0, -1): print(i)
+
+# 11021번
+testcnt = int(input())
+for i in range(1,testcnt+1):
+    a,b = map(int, input().split())
+    print(f'Case #{i}: {a+b}')
+
+# 11022번
+testcnt = int(input())
+for i in range(1,testcnt+1):
+    a,b = map(int, input().split())
+    print(f'Case #{i}: {a} + {b} = {a+b}')
+    
+# 2438번
+for dot in range(1, int(input())+1): print('*' * dot)
+'''
+
+# 2439번
+
+
+
+
+
+
